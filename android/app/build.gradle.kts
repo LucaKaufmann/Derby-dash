bb0e870e1f6bbfa410cd884db3c98dbfb606bf7b
Luca <lucakaufmann92@gmail.com>
2026-02-05 22:13:02 +0200

    Set Android applicationId

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.derbydash.derby_dash"
+        applicationId = "com.codable.derbydash"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
